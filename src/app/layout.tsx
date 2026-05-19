bd7ac4331995308eb21c46a75f967b8753de9180
Eduardo Roa U <35941081+eduardoroa22@users.noreply.github.com>
2026-05-18 21:59:44 -0500

    Refactor Matomo tracking implementation in RootLayout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,19 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import Script from "next/script";
 import "./globals.css";
+
+const MATOMO_SNIPPET = `
+  var _paq = window._paq = window._paq || [];
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//analytics.inglesbienhablado.com/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '6']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
+`;
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,22 +41,22 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        {/* Matomo */}
+        <script dangerouslySetInnerHTML={{ __html: MATOMO_SNIPPET }} />
+        {/* End Matomo Code */}
+      </head>
       <body className="min-h-full flex flex-col">
-        <Script id="matomo" strategy="afterInteractive">
-          {`
-            var _paq = window._paq = window._paq || [];
-            _paq.push(['trackPageView']);
-            _paq.push(['enableLinkTracking']);
-            (function() {
-              var u="//analytics.inglesbienhablado.com/";
-              _paq.push(['setTrackerUrl', u+'matomo.php']);
-              _paq.push(['setSiteId', '6']);
-              var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-              g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-            })();
-          `}
-        </Script>
         {children}
+        {/* Matomo Image Tracker (noscript fallback) */}
+        <noscript>
+          <img
+            referrerPolicy="no-referrer-when-downgrade"
+            src="https://analytics.inglesbienhablado.com/matomo.php?idsite=6&rec=1"
+            style={{ border: 0 }}
+            alt=""
+          />
+        </noscript>
       </body>
     </html>
   );
